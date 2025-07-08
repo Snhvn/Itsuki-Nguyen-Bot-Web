049f440e00f41690ce576d54f9975d2798bd4609
Snhvn <snhvnnguyen@gmail.com>
2025-07-08 20:21:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (key) {
         keyDisplay.style.display = 'block'; // Hiển thị div khi có key
-        keyDisplay.innerHTML = `Key Tool: <strong>${key}</strong>`;
+        keyDisplay.innerHTML = `<strong>${key}</strong>`;
     }
 });
 
